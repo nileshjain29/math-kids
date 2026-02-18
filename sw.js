5b27b2db3613ecb86ffe4e6cb6b50340d4d5ff22
nileshjain29 <61725436+nileshjain29@users.noreply.github.com>
2026-02-18 15:29:28 -0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "mathkids-v7";
+const CACHE_NAME = "mathkids-v9";
 const ASSETS = [
   ".",
   "index.html",
@@ -33,8 +33,14 @@ self.addEventListener("activate", (event) => {
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches
-      .match(event.request)
-      .then((cached) => cached || fetch(event.request)),
+    fetch(event.request)
+      .then((response) => {
+        const clone = response.clone();
+        caches
+          .open(CACHE_NAME)
+          .then((cache) => cache.put(event.request, clone));
+        return response;
+      })
+      .catch(() => caches.match(event.request)),
   );
 });
